0f3f5055b8013d18b857c5eb8b3a3e7629473029
Rvn-A19 <service2328@rambler.ru>
2019-08-17 20:13:03 +0300

    Use wide strings. Add gitignore.

--- winhello.cc
+++ winhello.cc
@@ -17,49 +17,49 @@ const DWORD kBufferSize = 512;
 
 class PathJoin {
 public:
-  PathJoin(std::initializer_list<const std::string> components);
-  std::string GetResultPath();
+  PathJoin(std::initializer_list<const std::wstring> components);
+  std::wstring GetResultPath();
 
 private:
-  std::string result_path;
+  std::wstring result_path;
 };
 
 
-bool EndsWith(std::string arg, char sym) {
+bool EndsWith(std::wstring arg, wchar_t sym) {
   return arg[arg.size() - 1] == sym;
 }
 
 
-PathJoin::PathJoin(std::initializer_list<const std::string> components) {
+PathJoin::PathJoin(std::initializer_list<const std::wstring> components) {
   auto it = components.begin() + 1;
   result_path = *components.begin();
   for (; it + 1 != components.end(); it ++) {
-    if (!EndsWith(result_path, '\\')) {
-      result_path += "\\";
+    if (!EndsWith(result_path, L'\\')) {
+      result_path += L"\\";
     }
     result_path += *it;
   }
-  if (!EndsWith(result_path, '\\')) {
-    result_path += "\\";
+  if (!EndsWith(result_path, L'\\')) {
+    result_path += L"\\";
   }
   result_path += *it;
 }
 
 
-std::string PathJoin::GetResultPath() {
+std::wstring PathJoin::GetResultPath() {
   return result_path;
 }
 
 
-std::string GetNameBySid(PSID target_sid) {
-  std::string result;
-  auto user = std::make_unique<char[]>(kBufferSize);
+std::wstring GetNameBySid(PSID target_sid) {
+  std::wstring result;
+  auto user = std::make_unique<WCHAR[]>(kBufferSize);
   DWORD user_len = kBufferSize;
-  auto domain = std::make_unique<char[]>(kBufferSize);
+  auto domain = std::make_unique<WCHAR[]>(kBufferSize);
   DWORD domain_len = kBufferSize;
   SID_NAME_USE sid_name_use;
-  if (LookupAccountSid(nullptr, target_sid, user.get(), &user_len, domain.get(), &domain_len, &sid_name_use)) {
-    result.assign(user.get()).append("@").append(domain.get());
+  if (LookupAccountSidW(nullptr, target_sid, user.get(), &user_len, domain.get(), &domain_len, &sid_name_use)) {
+    result.assign(user.get()).append(L"@").append(domain.get());
   }
   return result;
 }
@@ -72,25 +72,25 @@ void ShowCurrentProcessSid() {
       [](HANDLE *myhandle) { CloseHandle(*myhandle); } };
 
   if (!OpenProcessToken(token, TOKEN_READ, &token)) {
-    fprintf(stderr, "Failed to open process token (%i)\n", GetLastError());
+    fwprintf(stderr, L"Failed to open process token (%i)\n", GetLastError());
     return;
   }
   DWORD total_return_length = 0;
   auto data_buffer = std::make_unique<BYTE[]>(kBufferSize);
   if (!GetTokenInformation(token, TokenUser, data_buffer.get(), kBufferSize, &total_return_length)) {
-    fprintf(stderr, "Unable to get token information (%i)\n", GetLastError());
+    fwprintf(stderr, L"Unable to get token information (%i)\n", GetLastError());
     return;
   }
   
   PTOKEN_USER token_info = (PTOKEN_USER)data_buffer.get();
-  LPSTR string_sid;
+  LPWSTR string_sid;
 
-  if (!ConvertSidToStringSid(token_info->User.Sid, &string_sid)) {
+  if (!ConvertSidToStringSidW(token_info->User.Sid, &string_sid)) {
     fprintf(stderr, "Convert to string sid error (%i)\n", GetLastError());
     return;
   }
 
-  std::cout << "Process user sid is " << string_sid << "(" << GetNameBySid(token_info->User.Sid) << ")" << std::endl;
+  std::wcout << "Process user sid is " << string_sid << "(" << GetNameBySid(token_info->User.Sid) << ")" << std::endl;
 
   LocalFree(string_sid);
 
@@ -98,11 +98,11 @@ void ShowCurrentProcessSid() {
 
 
 int main() {
-  std::cout << "Hello, World!\n";
-  std::string x;
-  x.assign("test");
-  std::cout << "string is " << x << std::endl;
-  std::cout << PathJoin( { "D:", "users", "group.txt", x } ).GetResultPath() << std::endl;
+  std::wcout << L"Hello, World!\n";
+  std::wstring x;
+  x.assign(L"test");
+  std::wcout << L"string is " << x << std::endl;
+  std::wcout << PathJoin( { L"D:", L"users", L"group.txt", x } ).GetResultPath() << std::endl;
   ShowCurrentProcessSid();
   return 0;
 }
